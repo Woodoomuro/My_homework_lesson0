d0b6769cbf288313daa28063c12ac66f27366383
Dmitry <woodoomuro@gmail.com>
2024-11-06 14:40:46 +0300

    Add files via upload

---
+++ NewProject.py
@@ -0,0 +1,17 @@
+print("1st program")
+print(9**0.5)
+print(3*5)
+
+print("2nd program")
+print(9.99>9.98 and 1000!=1000.1)
+
+print("3rd program")
+print(2*2+2)
+print(2*(2+2))
+print(2*2+2==2*(2+2))
+
+print("4th program")
+print(float('123.456'))
+print(123.456*10)
+print(int(1234.56))
+print(1234%10)
